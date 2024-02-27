c39f2f30196db3f537a4255020adc54a359fd8b8
Giotu <evilalex1998@mail.ru>
2024-02-27 23:25:18 +0300

    feat: resolve task mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const obj = {};
+
+  objects.forEach((object) => {
+    Object.entries(object).forEach(([key, value]) => {
+      if (key in obj) {
+        obj[key] += value;
+      } else obj[key] = value;
+    });
+  });
+
+  return obj;
 }
 
 /**
